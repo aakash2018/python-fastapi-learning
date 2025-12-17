89487b42ba3cf39bb826d069b15aed2053ce2056
aakash <aakashprajapat2020@gmail.com>
2025-12-17 23:14:47 +0530

    project 2 setup

---
+++ project2/app/task/services.py
@@ -0,0 +1,64 @@
+from sqlmodel import Session, select
+from app.task.model import Task, TaskUpdate, TaskPatch, TaskOut
+from app.db.config import engine
+from fastapi import HTTPException
+
+
+def create_task(session: Session, new_task: Task) -> TaskOut:
+    task = Task(title=new_task.title, content=new_task.content)
+    with Session(engine) as session:
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+        return task
+
+
+def get_all_tasks(session: Session) -> list[TaskOut]:
+    with Session(engine) as session:
+        stmt = select(Task)
+        tasks = session.exec(stmt)
+        return tasks.all()
+
+
+def get_task_by_id(session: Session, task_id: int) -> TaskOut:
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        return task
+
+
+def update_task(session: Session, task_id: int, new_task: TaskUpdate) -> TaskOut:
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        task_data = new_task.model_dump()
+        task.sqlmodel_update(task_data)
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+        return task
+
+
+def patch_task(session: Session, task_id: int, new_task: TaskPatch) -> TaskOut:
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        task_data = new_task.model_dump(exclude_unset=True)
+        task.sqlmodel_update(task_data)
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+        return task
+
+
+def delete_task(session: Session, task_id: int):
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        session.delete(task)
+        session.commit()
+        return {"ok": True}
